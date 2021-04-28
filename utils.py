556b96d8ae8a029407a1a7cdc03eb744913d0ce4
sekR4 <sebastian.kraus.jena@gmail.com>
2021-04-28 20:56:18 +0200

    probably waisted much time with an R-like summary

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from scipy import stats
 
 columns = [
     "symboling",
@@ -49,7 +50,7 @@ numerics = [
 ]
 
 
-def clean_df(df: pd.DataFrame):
+def clean_df(df: pd.DataFrame) -> pd.DataFrame:
     """Cleans the DataFrame as suggested in 'machine learning task.pdf'
     
     - Replaces '?' with NaNs
@@ -95,3 +96,61 @@ def clean_df(df: pd.DataFrame):
     ), "DataFrame still contains missing values"
 
     return tmp
+
+
+def summary(model, X: pd.DataFrame, y: pd.Series):
+    """R-like summary of a linear regression model
+    
+    Inspired by 
+    https://stackoverflow.com/questions/27928275/find-p-value-significance-in-scikit-learn-linearregression
+
+    Parameters
+    ----------
+    model : LinearRegression()
+        A fitted regression model from sklearn e.g. LinearRegression().fit(X, y)
+    X : pd.DataFrame
+        Features
+    y : pd.Series
+        Target variable
+    """
+
+    lm, features = model, list(X.columns)
+    features.insert(0, "(Intercept)")
+    params, y_hat = np.append(lm.intercept_, lm.coef_), lm.predict(X)
+
+    newX = pd.DataFrame({"Constant": np.ones(len(X))}).join(
+        pd.DataFrame(X.reset_index(drop=True))
+    )
+    MSE = (sum((y - y_hat) ** 2)) / (len(newX) - len(newX.columns))
+
+    var_b = MSE * (np.linalg.inv(np.dot(newX.T, newX)).diagonal())
+    sd_b = np.sqrt(var_b)
+    ts_b = params / sd_b
+
+    p_values = [2 * (1 - stats.t.cdf(np.abs(i), (len(newX) - 1))) for i in ts_b]
+
+    sd_b = np.round(sd_b, 3)
+    ts_b = np.round(ts_b, 3)
+    p_values = np.round(p_values, 3)
+    params = np.round(params, 4)
+
+    result = pd.DataFrame()
+
+    result[" "] = features
+    (
+        result["Coefficients"],
+        result["Std. Error"],
+        result["t value"],
+        result["Pr(>|t|)"],
+    ) = [params, sd_b, ts_b, p_values]
+
+    # NOTE: Let's assume there are no transformations on y (e.g. log).
+    # Otherwise the formula printed is wrong.
+    print("Call:")
+    print(f"lm(formula = {y.name} ~ {' + '.join(features[1:])})", "\n")
+    pd.set_option("display.max_rows", None)
+    print(result, "\n")
+    # print(result[result["Pr(>|t|)"].notna()].sort_values(by="Pr(>|t|)"), "\n")
+    pd.set_option("display.max_rows", 10)
+    print("R squared: ", round(model.score(X, y), 4), "\n")
+
